83bdb89de60d424174723ed06fbf6437fad71965
Ruchita Bhupatbhai Patel <rxp1326@mavs.uta.edu>
2022-08-08 18:12:01 -0500

    modified UI

--- ServeMe/ServeMe/ClientApp/src/app/reviews-ratings/reviews-ratings.component.ts
+++ ServeMe/ServeMe/ClientApp/src/app/reviews-ratings/reviews-ratings.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ReviewsRatingsComponent implements OnInit {
   stars: number[] = [1, 2, 3, 4, 5];
   selectedValue: number;
+  comment = ''
   constructor() { }
 
   ngOnInit(): void {
